13b02f346275e0db408a7a5f4632194e89bf31ef
Seth Kingsbury <sethkingsbury@Seths-MacBook-Air.local>
2025-05-05 20:46:34 +1200

    create contact form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,7 +3,21 @@ import "../styles/contact.css";
 export default function Contact() {
     return (
         <div className="contact-container">
-            <h1>Contact</h1>
+            <h1>Contact Us</h1>
+            <form>
+                <div className="form-row">
+                    <input type="text" placeholder="I'd like to ask about..." />
+                </div>
+
+                <div className="form-row">
+                    <input className="margin-right-big-screen" type="text" placeholder="Name"/>
+                    <input className="margin-left-big-screen" type="text" placeholder="Email"/>
+                </div>
+
+                <div className="form-row">
+                    <textarea placeholder="Message..."></textarea>
+                </div>
+            </form>
         </div>
     );
 }
